d76c44539998388f1d142d136559c8610d55ecdc
Snolledz99 <Prakarnz19@gail.com>
2021-12-17 11:08:53 +0700

    git push

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ButtonList from './ButtonList';
+import Menu from './Menu.js';
+
+ 
+function App() {
+  return (
+    <div className="App">
+    <Menu />
+    <ButtonList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Button = ({ defaultBtn, text, iconstart, iconend, cName }) => {
+  return (
+    <>
+      <div className='Btn-content'>
+        <p>{`${defaultBtn ? defaultBtn : ''}`}</p>
+        <button className={`btn ${cName ? cName : ''}`}>
+          <span className='material-icons'> {iconstart ? iconstart : ''}</span>
+          {text ? text : 'Default'}
+          <span className='material-icons'>{iconend ? iconend : ''}</span>
+        </button>
+        
+      </div>
+    </>
+  )
+}
+
+export default Button
